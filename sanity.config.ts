3d947ca8341e416dd3fa26a15e37da796e88fb46
amethystc <pj.utami41@gmail.com>
2025-10-21 15:43:02 +0700

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Unknown project',
+
+  projectId: 'cokqkg27s1wfqc8t3q8ozd06',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
